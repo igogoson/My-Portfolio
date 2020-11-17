d2ddcb8c04226e33ea7661aa4b235ff6d04801f1
Igogoson <sospeterwangai1@gmail.com>
2020-11-17 22:55:29 +0300

    Changes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,8 @@
   <footer class="footer">
     <div>
         <font-awesome-icon class="copyright-icon" icon="copyright"/>
-        2020 {{user.name}}!
+        2020 {{user.name}}
+        | Get this template <a href="https://github.com/hbollon/portfolio-vuejs" target="_blank">here</a> !
     </div>
     <SocialBar :links="links"/>
   </footer>
